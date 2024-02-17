d7a949b74695475776143c82c5b9023469c8ec07
Sanjoy Garai <sanjoygarai2002@gmail.com>
2024-02-17 16:05:14 +0530

    Contest Codes

---
+++ A_Sasha_and_the_Beautiful_Array.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include <deque>
+//#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define pp pop_back
+#define ll long long
+#define ld long double
+#define int long long
+#define pair pair<int,int>
+typedef vector<ll> vi;
+#define forall(i,n,x) for(int i=0; i<n; i+=x)
+#define all(x) x.begin(), x.end()
+
+void solve()
+{
+    int n, sum = 0;
+    cin >> n;
+     vi a(n);
+     forall(i,n,1) cin >> a[i];
+     sort(all(a),greater<int>());
+     forall(i,n-1,1) sum += (a[i]-a[i+1]);
+     cout << sum << endl;
+}
+
+signed main()
+{
+ios::sync_with_stdio(false);
+cout.tie(0); cin.tie(0);
+int t = 1;
+cin >> t;
+while(t--)
+{
+solve();
+}
+
+return 0;
+}
---
+++ C_Sasha_and_the_Casino.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include <deque>
+//#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define pp pop_back
+#define ll long long
+#define ld long double
+#define int long long
+#define pair pair<int,int>
+typedef vector<ll> vi;
+#define forall(i,n,x) for(int i=0; i<n; i+=x)
+#define all(x) x.begin(), x.end()
+
+void solve()
+{
+    int k,x,a; cin>>k>>x>>a;
+    if(a<=x) {cout<<"NO\n";return;}
+
+    vi aahh(x+1,0);
+    aahh[0]=1;
+    
+    ll limit=1;
+    a-=1;
+
+    for(int i=1;i<=x;i++)
+    {
+        int required=ceil(1.0*(limit+1)/(k-1));
+        aahh[i]=required;
+        a-=required;
+        if(a<0) {cout<<"NO\n"; return;}
+        limit+=required;
+    }
+
+    cout<<"YES\n";
+    return;
+
+}
+
+
+signed main()
+{
+ios::sync_with_stdio(false);
+cout.tie(0); cin.tie(0);
+int t = 1;
+cin >> t;
+while(t--)
+{
+solve();
+}
+
+return 0;
+}
